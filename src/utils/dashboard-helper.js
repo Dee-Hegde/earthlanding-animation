295e138edf85b757c60dd91738f78445e9c119e4
deepakhegde1 <deepakhegde@sketchbrahma.com>
2024-01-02 15:42:34 +0530

    infinite scroll animation added

--- src/utils/dashboard-helper.js
+++ src/utils/dashboard-helper.js
@@ -2,7 +2,8 @@ import landingcat from '../assets/images/banners/landingcat.png';
 import flyDora from '../assets/images/doraemon-jump/flydora.png';
 import desert from '../assets/images/banners/desert.png';
 import scrollIndicator from '../assets/images/banners/scrollind.png';
-import cricketers from "../assets/images/banners/cricketers.png"
+import cricketers from "../assets/images/banners/cricketers.png";
+import infiniteScroll from "../assets/images/banners/InfiniteScroll.png"
 
 export const dashboardImages = {
   trending: landingcat,
@@ -11,4 +12,5 @@ export const dashboardImages = {
   desert,
   scrollIndicator,
   cricketers,
+  infiniteScroll,
 };
